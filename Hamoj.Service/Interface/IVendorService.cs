3c859b0a72900281ba7a2219770e87e60675c8a4
Hetal <hetaljivani216@gmail.com>
2024-05-23 19:25:08 +0530

    Vendor Data Bind

---
+++ Hamoj.Service/Interface/IVendorService.cs
@@ -0,0 +1,10 @@
+﻿
+using Hamoj.Service.Dto;
+
+namespace Hamoj.Service.Interface;
+
+public interface IVendorService
+{
+    Task<List<VendorDto>> GetAllAsync();
+
+}
---
+++ Hamoj.Service/Services/VendorService.cs
@@ -0,0 +1,30 @@
+﻿using Hamoj.DB.Context;
+using Hamoj.Service.Dto;
+using Hamoj.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hamoj.Service.Services;
+
+public class VendorService : IVendorService
+{
+    private readonly HamojDBContext _context;
+    public VendorService(HamojDBContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<VendorDto>> GetAllAsync()
+    {
+        var data = await _context.Vendor.Select(a=>new VendorDto
+        {
+            Id = a.Id,
+            Name = a.Name,
+            Email = a.Email,
+            Phone = a.Phone,
+            Contact_Phone = a.Contact_Phone,
+            Address = a.Address,
+
+        }).ToListAsync();
+        return data;
+    }
+}
