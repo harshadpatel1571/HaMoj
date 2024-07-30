5b1b83c4666305257ed745716bb4803e9bf7a9f5
SahilBoda07 <sahilboda07@gmail.com>
2024-07-30 13:42:42 +0530

    App file

--- HamojApp/obj/Release/net8.0-windows10.0.19041.0/win10-x64/HamojApp.AssemblyInfo.cs
+++ HamojApp/obj/Release/net8.0-windows10.0.19041.0/win10-x64/HamojApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HamojApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f3329c5d057ee3ac06e8b459ca0e3984b01f33f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+903fb69f58c3c9a97284bb49b9ef73e0ae9e9ed9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HamojApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HamojApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
