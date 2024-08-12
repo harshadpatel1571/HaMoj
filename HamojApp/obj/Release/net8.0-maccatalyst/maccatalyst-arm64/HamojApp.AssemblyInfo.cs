cc7356f9d162800134629d5a720d7ca5f4209d46
SahilBoda07 <sahilboda07@gmail.com>
2024-08-12 13:13:44 +0530

    Customer Register

--- HamojApp/obj/Release/net8.0-maccatalyst/maccatalyst-arm64/HamojApp.AssemblyInfo.cs
+++ HamojApp/obj/Release/net8.0-maccatalyst/maccatalyst-arm64/HamojApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HamojApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f3329c5d057ee3ac06e8b459ca0e3984b01f33f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5b1b83c4666305257ed745716bb4803e9bf7a9f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HamojApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HamojApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
